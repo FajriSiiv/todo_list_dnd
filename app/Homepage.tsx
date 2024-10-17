9902cbbe6b696ab77518f67bc297d84fca4dbada
Muhammad Fajri <fajrijijin@gmail.com>
2024-10-17 20:07:30 +0700

    adding button to github repository

--- app/Homepage.tsx
+++ app/Homepage.tsx
@@ -1,8 +1,16 @@
 "use client";
 
-import FormTodo from "@/components/forms/FormTodo";
 import { ModalFormTodo } from "@/components/forms/ModalFormTodo";
 import ResultTodo from "@/components/ResultTodo";
+import { Button } from "@/components/ui/button";
+import { DialogTrigger } from "@/components/ui/dialog";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { GithubIcon } from "lucide-react";
 import React, { useEffect, useState } from "react";
 
 export const uniqueStoreLocalStorage = "siiv_todo_dnd";
@@ -33,8 +41,28 @@ export default function Homepage() {
     }
   }, []);
 
+  const handleUrlGithub = () => {
+    window.open("https://github.com/FajriSiiv/todo_list_dnd", "_blank");
+  };
+
   return (
-    <div className="p-10">
+    <div className="p-10 relative">
+      <TooltipProvider>
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <Button
+              size="icon"
+              className="fixed left-5 bottom-5"
+              onClick={handleUrlGithub}
+            >
+              <GithubIcon />
+            </Button>
+          </TooltipTrigger>
+          <TooltipContent side="right">
+            <p>FajriSiiv - Repository Github</p>
+          </TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
       <div className="">
         <h1 className="font-bold text-xl text-center pb-2">Todo-List</h1>
         <ModalFormTodo
